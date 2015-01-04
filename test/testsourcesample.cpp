ce28059407756f3b917322522e890b395aa53b35
Михаил Юрьевич <zv@revolution.Dlink>
2015-01-05 00:39:45 +0300

    More hard test strings added

--- test/testsourcesample.cpp
+++ test/testsourcesample.cpp
@@ -8,11 +8,15 @@
 
 int main()
 {
-    // Cyrillic UTF8 strings
+    // Cyrillic UTF8 strings:
+    // First and second examples works correctly.
     QString s1 = QObject::trUtf8( "Строка1" );
     QString s2 = QObject::trUtf8("Prefix2") + QObject::trUtf8( "Строка21" + "Строка22");
     // Third example not works. It also not supported by Qt lupdate.
     QString s3 = QObject::trUtf8( "Строка31" + DEF3 + "Строка33");
+    // Fourth example works correctly.
+    QString s4 = QObject::trUtf8( "Строка41" \
+       "Строка42");
     return 0;
 }
 
