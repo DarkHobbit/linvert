9b4fab0f49f0549facb3db4c640f480b5c6c6f52
Михаил Юрьевич <zv@revolution.Dlink>
2015-01-18 20:42:32 +0300

    License included in source code; draft for Russian manual included

--- linvertapplication.h
+++ linvertapplication.h
@@ -1,3 +1,20 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 Mikhail Y. Zvyozdochkin aka DarkHobbit
+** Contact: pub@zvyozdochkin.ru
+**
+** This file is part of the LInvert utility for Qt
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public License
+** version 2.1 or version 3 as published by the Free Software Foundation.
+** Please review the ** following information to ensure the
+** GNU Lesser General Public License requirements will be met:
+** https://www.gnu.org/licenses/lgpl.html and
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
+
 #ifndef LINVERTAPPLICATION_H
 #define LINVERTAPPLICATION_H
 
